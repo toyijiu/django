e9b6f674ebfa1d3cf5c8c078250f2b37fbc060f5
WuXiao <309378714@qq.com>
2019-02-11 16:40:22 +0800

    Initial commit,push the origin files

---
+++ mysite/polls/views.py
@@ -0,0 +1,43 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse,HttpResponseRedirect
+from django.template import loader
+from .models import Question,Choice
+from django.shortcuts import render,get_object_or_404
+from django.urls import reverse
+from django.db.models import F
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        "latest_question_list":latest_question_list,
+    }
+    return render(request,'polls/index.html',context)
+
+def detail(request,question_id):
+    question = get_object_or_404(Question,pk=question_id)
+    return render(request,"polls/detail.html",{'question':question})
+
+def results(request,question_id):
+    question = get_object_or_404(Question,pk=question_id)
+    return render(request,'polls/results.html',{'question':question})
+
+def vote(request,question_id):
+    question = get_object_or_404(Question,pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except(KeyError,Choice.DoesNotExist):
+        context = {
+            'question':question,
+            'error_message':"You did not select a choice.",
+        }
+        return render(request,'polls/detail.html',context)
+    else:
+        #selected_choice.votes += 1
+        selected_choice.votes = F('votes') + 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results',args=(question.id,)))
+
+
